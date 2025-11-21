2fb04a9b995240c216f2bfc5a494afb8dc6b1be2
thuyps <thuyps@yahoo.com>
2025-11-21 22:20:45 +0700

    symbolic link

---
+++ symboliclink.cmd
@@ -0,0 +1,4 @@
+# 1. remove target folder:
+#rmdir "C:\Users\Chau Anh\AppData\Roaming\MetaQuotes\Terminal\53785E099C927DB68A545C249CDBCE06\MQL5\Experts\Advisors"
+
+mklink /D "C:\Users\Chau Anh\AppData\Roaming\MetaQuotes\Terminal\53785E099C927DB68A545C249CDBCE06\MQL5\Experts\Advisors" "C:\Data\alpha007\Experts\Advisors"
